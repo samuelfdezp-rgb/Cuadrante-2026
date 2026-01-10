47de8da4b58381c0a7b6a0420615cd053e32efeb
samuelfdezp-rgb <samuelfdezp@gmail.com>
2026-01-10 05:31:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -200,7 +200,7 @@ def estilo_turno(t):
         return {"bg": "#00B050", "fg": "#FF0000", "bold": True}
 
     bg, fg = base.get(t, ("#FFFFFF", "#000000"))
-    return {"bg": bg, "fg": fg, "bold": t == "perm"}
+    return {"bg": bg, "fg": fg, "bold": t == "perm" and "Ts"}
 
 # ==================================================
 # PESTAÑAS
